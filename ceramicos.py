aab36764616a258a0f3fe37e8f3181b7293dcc19
matias1998za <matias1998za@gmail.com>
2020-09-11 19:13:20 -0300

    A mitad de camino

---
+++ ceramicos.py
@@ -0,0 +1,8 @@
+F=int(input("Ingrese 1  si hay cliente, 2 si no hay"))
+if F==1:
+    L=int(input("Ingresar largo de habitaciÃƒÂ³n"))
+    A=int(input("Ingresar ancho de habitaciÃƒÂ³n"))
+    ETN=L*A
+    LC=int(input("Ingresar largo de ceramico"))
+    AC=int(input("Ingresar ancho de ceramico"))
+    ETM=LC*AC
